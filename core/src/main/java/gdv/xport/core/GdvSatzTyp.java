01e1a76181c2638564083a1dbdfbd1061be881b9
Oli B <o.boehm@optica.de>
2021-02-16 07:44:59 +0100

    GdvSatzTyp aus GdvSatz extrahiert

---
+++ core/src/main/java/gdv/xport/core/GdvSatzTyp.java
@@ -0,0 +1,441 @@
+/*
+ * Copyright (c) 2013-2021 by Oli B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 26.01.2013 by Oli B. (ob@aosd.de)
+ */
+
+package gdv.xport.core;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Der GdvSatzTyp fuehrt Satzart, Sparte, Wagnisart und laufende Nummer eines
+ * Teildatensatz zusammen. Sie wird u.a. von der SatzFactory fuer die
+ * Registrierung verwendet und wurde mit v5 aus SatzTyp herausausgezogen.
+ * <p>
+ * Vorher hiess diese Klasse "SatzNummer", wurde aber mit 1.1 in SatzType
+ * umbenannt, da "Satznummer" als Klassenname etwas irritierend ist, da es ein
+ * Feld "Satznummer" innerhalb eines Satzes bereits gibt.
+ * </p>
+ *
+ * @author oliver
+ */
+public class GdvSatzTyp {
+
+	/** The satzart. */
+	private final int satzart;
+
+	/** The sparte. */
+	private final int sparte;
+
+	/** The wagnisart. */
+	private final int wagnisart;
+
+	/** Kranken, Folgenummer */
+	private final int krankenFolgeNr;
+
+	/** The lfd nummer. */
+	private final int teildatensatzNummer;
+
+	/** Bausparen, bausparenArt */
+	private final int bausparenArt;
+
+	/**
+	 * Damit laesst sich ein SatzTyp anhand der entsprechenden String-
+	 * Repraesentation erzeugen.
+	 *
+	 * @param nr z.B. "0210.050"
+	 * @return der entsprechende SatzTyp
+	 * @since 5.0
+	 */
+	public GdvSatzTyp(String nr) {
+		this(toNumbers(nr));
+	}
+
+	private GdvSatzTyp(int[] n) {
+		this(n[0], n[1], n[2], n[3], n[4], n[5]);
+	}
+
+	private static int[] toNumbers (String nr)  {
+		int[] numbers = { -1, -1, -1, -1, -1, -1 };
+		try {
+			String[] parts = StringUtils.split(nr, '.');
+			for (int i = 0; i < parts.length; i++) {
+				numbers[i] = Integer.parseInt(parts[i]);
+			}
+			if (numbers[1] == 20) {
+				// bei Kranken muss krankenFolgeNr belegt werden
+				numbers[3] = numbers[2];
+				numbers[2] = -1;
+				numbers[4] = -1;
+				numbers[5] = -1;
+			} else if (numbers[1] == 580) {
+				// bei Bausparen muss bausparenArt belegt werden
+				numbers[5] = numbers[2];
+				numbers[2] = -1;
+				numbers[3] = -1;
+				numbers[4] = -1;
+			} else {
+				numbers[4] = numbers[3];
+				numbers[3] = -1;
+				numbers[5] = -1;
+			}
+			return numbers;
+		} catch (NumberFormatException ex)  {
+			throw new IllegalArgumentException("kein Satz-Typ: '" + nr + "'", ex);
+		}
+	}
+
+	/**
+     * Instantiates a new satz nummer.
+     *
+     * @param satzart the satzart
+     */
+    public GdvSatzTyp(final int satzart) {
+      this(satzart, -1);
+    }
+
+    /**
+     * Instantiates a new satz nummer.
+     *
+     * @param satzart Satzart
+     * @param sparte Sparte
+     */
+    public GdvSatzTyp(final int satzart, final int sparte) {
+      this(satzart, sparte, -1);
+    }
+
+    /**
+     * Instantiiert einen neuen SatzTyp.
+	 * TODO: wird mit v6 entfernt werden
+     *
+     * @param satzart the satzart
+     * @param sparte the sparte
+     * @param artFolgeNr Wagnisart (Sparte 10) bzw. krankenFolgeNr (Sparte 20)
+     *          bzw. bausparenArt (Sparte 580, Satzart 220 (Wert 1 - 2))
+     */
+    protected GdvSatzTyp(final int satzart, final int sparte, final int artFolgeNr) {
+      this(satzart, sparte, artFolgeNr, -1);
+    }
+
+    /**
+     * Legt eine neuen SatzTyp an.
+	 * TODO: Wird ab v6 nicht mehr zur Verfuegung stehen.
+     *
+     * @param satzart die Satzart (vierstellig)
+     * @param sparte die Sparte (dreistellig)
+	 * @param artFolgeNr Wagnisart (Sparte 10) bzw. krankenFolgeNr (Sparte 20) bzw. bausparenArt (Sparte 580, Satzart 220 (Wert 1 - 2))
+     * @param lfdNummer die laufende Nummer (Teildatensatz-Nummer)
+     */
+	protected GdvSatzTyp(final int satzart, final int sparte, final int artFolgeNr, final int lfdNummer) {
+		this(satzart, sparte, (sparte == 20) || (sparte == 580) ? -1 : artFolgeNr, (sparte == 20) ? artFolgeNr : -1,
+				lfdNummer, (sparte == 580) ? artFolgeNr : -1);
+	}
+
+    /**
+     * Legt eine neue SatzNummer an.
+	 * TODO: Wird ab v6 nicht mehr zur Verfuegung stehen.
+     *
+     * @param satzart die Satzart (vierstellig)
+     * @param sparte die Sparte (dreistellig)
+     * @param wagnisart die Wagnisart (ein- bis zweisstellig)
+     * @param krankenFolgeNr Folge-Nr. aus Sparte 20, Satzart 220 (Wert 1-3)
+     * @param lfdNummer die laufende Nummer (Teildatensatz-Nummer)
+     */
+    protected GdvSatzTyp(final int satzart, final int sparte, final int wagnisart, final int krankenFolgeNr, final int lfdNummer) {
+      this(satzart, sparte, wagnisart, krankenFolgeNr, lfdNummer, -1);
+    }
+
+	/**
+	 * Legt eine neue SatzNummer an.
+	 *
+	 * @param satzart die Satzart (vierstellig)
+	 * @param sparte die Sparte (dreistellig)
+	 * @param wagnisart die Wagnisart (ein- bis zweisstellig)
+	 * @param krankenFolgeNr Folge-Nr. aus Sparte 20, Satzart 220 (Wert 1-3)
+	 * @param lfdNummer die laufende Nummer (Teildatensatz-Nummer)
+	 * @param bausparenArt die Art bei Sparte 580, Satzart 220 (Wert 1 - 2)
+	 * @since 4.X
+	 */
+	private GdvSatzTyp(final int satzart, final int sparte, final int wagnisart, final int krankenFolgeNr, final int lfdNummer, final int bausparenArt) {
+		assert (satzart >= 0) && (satzart <= 9999) : "Satzart " + satzart
+		        + " muss zwischen 0 und 9999 liegen";
+		assert (sparte == -1) || ((0 <= sparte) && (sparte <= 999)) : "Sparte " + sparte
+		        + " muss zwischen 0 und 999 liegen";
+		assert (wagnisart == -1) || ((0 <= wagnisart) && (wagnisart <= 9)) || (wagnisart == 13) || (wagnisart == 48) :
+				"Wagnisart " + wagnisart + " muss zwischen 0 und 9 liegen";
+		assert (krankenFolgeNr == -1) || ((1 <= krankenFolgeNr) && (krankenFolgeNr <= 3)) : "Kranken Folge-Nr. "
+		        + krankenFolgeNr + " muss zwischen 1 und 3 liegen";
+		assert (lfdNummer == -1) || ((0 <= lfdNummer) && (lfdNummer <= 9)) : "teildatensatzNummer "
+		        + lfdNummer + " muss zwischen 0 und 9 liegen";
+		assert (bausparenArt == -1) || ((0 <= bausparenArt) && (bausparenArt <= 9)) : "bausparenArt "
+		        + bausparenArt + " muss zwischen 0 und 9 liegen";
+		this.satzart = satzart;
+		this.sparte = ((satzart == 210 ) || (satzart == 211 ) || (satzart == 220 )) && (sparte < 0) ? 0 : sparte;
+		this.wagnisart = ((satzart == 220) && (sparte == 10) && (wagnisart < 0)) ? 0 : wagnisart;
+		this.krankenFolgeNr = krankenFolgeNr;
+		this.teildatensatzNummer = ((wagnisart > 0) && (lfdNummer < 0) && (sparte == 10)) ? 1 :  lfdNummer;
+		this.bausparenArt = bausparenArt;
+	}
+
+	/**
+	 * Gets the satzart.
+	 *
+	 * @return the satzart
+	 */
+	public int getSatzart() {
+		return this.satzart;
+	}
+
+	/**
+	 * Gets the sparte.
+	 *
+	 * @return the sparte
+	 */
+	public int getSparte() {
+		return this.sparte;
+	}
+
+  /**
+   * Liefert die Sparte als String.
+   *
+   * @return z.B. "030"
+   * @since 5.0
+   */
+  public String getSparteAsString()
+  {
+    return Integer.toString(this.getSparte());
+  }
+
+  /**
+   * Gets the wagnisart.
+   *
+   * @return the wagnisart
+   */
+  public int getWagnisart()   {
+    return this.wagnisart;
+  }
+
+	/**
+	 * Liefert die Wagnisart als String.
+	 *
+	 * @return z.B. "9"
+	 * @since 5.0
+	 */
+	public String getWagnisartAsString() {
+		return Integer.toString(this.getWagnisart());
+	}
+
+	/**
+	 * Liefert die BausparenArt zurueck. Dies ist bei SatzTyp "0220.580.01" der letzte
+	 * Teil ("01"). Diese Methode macht nur bei den Satz-Typen
+	 * "0220.580.01" und "0220.580.2" Sinn.
+	 *
+	 * @return z.B. 1 bei SatzTyp "0220.580.01"
+	 * @since 5.0
+	 */
+	public int getBausparenArt() {
+		return this.bausparenArt;
+	}
+
+	/**
+	 * Liefert die BausparenArt zurueck. Dies ist bei SatzTyp "0220.580.01" der letzte
+	 * Teil ("01"). Diese Methode macht nur bei den Satz-Typen
+	 * "0220.580.01" und "0220.580.2" Sinn.
+	 *
+	 * @return z.B. "01" bei SatzTyp "0220.580.01"
+	 * @since 5.0
+	 */
+	public String getBausparenArtAsString() {
+		if (this.getBausparenArt() < 0) {
+			return "";
+		}
+		if (this.getBausparenArt() == 1) {
+			return "01";
+		} else {
+			return Integer.toString(this.getBausparenArt());
+		}
+	}
+
+	/**
+	 * Liefert die Wagnisart, BausparenArt oder KrankenFolgeNr zurueck.
+	 * Dies ist der dritte Teil nach der Sparte, als z.B. die 0 bei
+	 * SatzTyp.of("0220.010.0").
+	 *
+	 * @return z.B. 1 bei SatzTyp "0220.580.01"
+	 * @since 5.0
+	 */
+	public int getArt() {
+		if (this.getSparte() == 10) {
+			switch (this.getWagnisart()) {
+				case 1:
+				case 3:
+					return 13;
+				case 4:
+				case 8:
+					return 48;
+				default:
+					return this.getWagnisart();
+			}
+		} else if (this.getSparte() == 20) {
+			return this.getKrankenFolgeNr();
+		} else if (this.getSparte() == 580) {
+			return this.getBausparenArt();
+		}
+		return -1;
+	}
+
+	/**
+	 * Liefert die Wagnisart, BausparenArt oder KrankenFolgeNr als String
+	 * zurueck. Dies ist der dritte Teil nach der Sparte, als z.B. die
+	 * "0" bei SatzTyp.of("0220.010.0").
+	 *
+	 * @return z.B. "01" bei SatzTyp "0220.580.01"
+	 * @since 5.0
+	 */
+	public String getArtAsString() {
+		if (this.getBausparenArt() == 1) {
+			return "01";
+		} else {
+			return Integer.toString(this.getArt());
+		}
+	}
+
+	/**
+	 * Liefert true oder false zurueck, je nachdem, ob der SatzTyp eine Art
+	 * hat. Dies ist z.B. bei den Satz-Typen 0220.580.01" und "0220.580.2"
+	 * der Fall.
+	 *
+	 * @return true oder false
+	 * @since 5.0
+	 */
+	public boolean hasArt() {
+		return (this.getWagnisart() >= 0) || (this.getBausparenArt() >= 0) || (this.getKrankenFolgeNr() >= 0);
+	}
+	
+	/**
+	 * Gets the krankenFolgeNr.
+	 * 
+	 * @return the krankenFolgeNr
+	 */
+	public int getKrankenFolgeNr() {
+	    return this.krankenFolgeNr;
+	}
+
+	/**
+	 * Dies ist die laufende Nummer bei der Wagnisart.
+	 *
+	 * @return the lfd nummer
+	 */
+	public int getTeildatensatzNummer() {
+		return this.teildatensatzNummer;
+	}
+
+	/**
+	 * Liefert true zurueck, wenn die Sparte gesetzt ist.
+	 *
+	 * @return true, if successful
+	 */
+	public boolean hasSparte() {
+		return this.getSparte() > 0;
+	}
+
+	/**
+	 * Liefert true zurueck, wenn die Wagnisart gesetzt ist.
+	 *
+	 * @return true, if successful
+	 */
+	public boolean hasWagnisart() {
+		return this.getWagnisart() >= 0;
+	}
+	
+	/**
+	 * Liefert true zurueck, wenn die Folge-Nr in Sparte 20, Satzart 220 gesetzt ist.
+	 * 
+	 * @return true, if successful
+	 */
+	public boolean hasKrankenFolgeNr() {
+	    return this.getKrankenFolgeNr() >= 0;
+	}
+
+    /**
+     * Liefert true zurueck, wenn die Bausparen-Artin Sparte 580, Satzart 220
+     * gesetzt ist.
+     * 
+     * @return true, if successful
+     */
+    public boolean hasBausparenArt()
+    {
+      return this.getBausparenArt() >= 0;
+    }
+
+    /**
+     * Liefert true zurueck, wenn die laufende Nummer (fuer Wagnisart) gesetzt
+     * ist.
+     *
+     * @return true, if successful
+     */
+    public boolean hasTeildatensatzNummer()  {
+      return this.getTeildatensatzNummer() >= 0;
+    }
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof GdvSatzTyp)) {
+			return false;
+		}
+		return this.toString().equals(obj.toString());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder();
+		buf.append(String.format("%04d", this.getSatzart()));
+		if (this.hasSparte()) {
+			buf.append(String.format(".%03d", this.getSparte()));
+			if (this.hasArt()) {
+				buf.append(".");
+				buf.append(this.getArtAsString());
+				if (this.getTeildatensatzNummer() >= 0) {
+					buf.append(".");
+					buf.append(this.getTeildatensatzNummer());
+				}
+			}
+		}
+		return buf.toString();
+	}
+
+}
---
+++ core/src/test/java/gdv/xport/core/GdvSatzTypTest.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (c) 2013 by Oli B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 26.01.2013 by Oli B. (ob@aosd.de)
+ */
+
+package gdv.xport.core;
+
+import org.junit.Test;
+import patterntesting.runtime.junit.ObjectTester;
+
+import static org.junit.Assert.*;
+import static patterntesting.runtime.junit.ObjectTester.assertNotEquals;
+
+/**
+ * JUnit-Tests fuer {@link GdvSatzTyp}.
+ *
+ * @author oliver
+ * @since 0.9 (26.01.2013)
+ */
+public class GdvSatzTypTest {
+
+    /**
+     * Test method for {@link GdvSatzTyp#equals(Object)}.
+     */
+    @Test
+    public void testEqualsObject() {
+        GdvSatzTyp one = new GdvSatzTyp(1);
+        GdvSatzTyp anotherOne = new GdvSatzTyp(1);
+        ObjectTester.assertEquals(one, anotherOne);
+    }
+
+    /**
+     * Test method for {@link GdvSatzTyp#equals(Object)}.
+     */
+    @Test
+    public void testNotEquals() {
+        GdvSatzTyp one = new GdvSatzTyp("1.1");
+        GdvSatzTyp other = new GdvSatzTyp("1.1.1");
+        assertNotEquals(other, one.equals(other));
+    }
+
+    /**
+     * Test to string.
+     */
+    @Test
+    public void testToString() {
+        assertEquals("0001", new GdvSatzTyp(1).toString());
+        assertEquals("0210.050", new GdvSatzTyp(210, 50).toString());
+        assertEquals("0220.010.0", new GdvSatzTyp(220, 10, 0).toString());
+        assertEquals("0220.010.6.1", new GdvSatzTyp(220, 10, 6, 1).toString());
+    }
+
+    @Test
+    public void testOf() {
+        assertEquals(new GdvSatzTyp("0001"), new GdvSatzTyp(1));
+        assertEquals(new GdvSatzTyp("0210.050"), new GdvSatzTyp(210, 50));
+        assertEquals(new GdvSatzTyp("0220.010.0"), new GdvSatzTyp(220, 10, 0));
+        assertEquals(new GdvSatzTyp("0220.010.6.1"), new GdvSatzTyp(220, 10, 6, 1));
+    }
+
+    @Test
+    public void testGetSatzart() {
+        assertEquals(210, new GdvSatzTyp("0210.050").getSatzart());
+    }
+
+    @Test
+    public void testGetSparte() {
+        assertEquals(50, new GdvSatzTyp("0210.050").getSparte());
+    }
+
+    @Test
+    public void testOfWagnisart1u3() {
+        assertEquals(new GdvSatzTyp("0220.010.13.7"), new GdvSatzTyp(220, 10, 1, 7));
+        assertEquals(new GdvSatzTyp("0220.010.13.7"), new GdvSatzTyp(220, 10, 3, 7));
+    }
+
+    /**
+     * GdvSatzTyp "0220.010.13" gibt es eigentlich nicht. Oft ist damit aber
+     * eigentlich GdvSatzTyp "0220.010.13.1" gemeint.
+     */
+    @Test
+    public void tesetOfWagnisartLeben() {
+        assertEquals(new GdvSatzTyp("0220.010.13.1"), new GdvSatzTyp("0220.010.13"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOfInvalid() {
+        new GdvSatzTyp("0001.a");
+    }
+
+    @Test
+    public void testGetArt() {
+        assertEquals("1", new GdvSatzTyp("0220.020.1").getArtAsString());
+        assertEquals("2", new GdvSatzTyp("0220.020.2").getArtAsString());
+        assertEquals("3", new GdvSatzTyp("0220.020.3").getArtAsString());
+        assertEquals("01", new GdvSatzTyp("0220.580.01").getArtAsString());
+        assertEquals("2", new GdvSatzTyp("0220.580.2").getArtAsString());
+    }
+
+    @Test
+    public void testGetArt4Wagnisart() {
+        assertEquals("0", new GdvSatzTyp("0220.010.0").getArtAsString());
+        assertEquals("13", new GdvSatzTyp("0220.010.13").getArtAsString());
+        assertEquals("13", new GdvSatzTyp("0221.010.13").getArtAsString());
+        assertEquals("48", new GdvSatzTyp("0220.010.48").getArtAsString());
+        assertEquals("48", new GdvSatzTyp("0221.010.48").getArtAsString());
+        assertEquals("6", new GdvSatzTyp("0220.010.6").getArtAsString());
+        assertEquals("6", new GdvSatzTyp("0221.010.6").getArtAsString());
+        assertEquals("5", new GdvSatzTyp("0220.010.5").getArtAsString());
+        assertEquals("5", new GdvSatzTyp("0221.010.5").getArtAsString());
+        assertEquals("2", new GdvSatzTyp("0220.010.2").getArtAsString());
+        assertEquals("2", new GdvSatzTyp("0221.010.2").getArtAsString());
+        assertEquals("7", new GdvSatzTyp("0220.010.7").getArtAsString());
+        assertEquals("7", new GdvSatzTyp("0221.010.7").getArtAsString());
+        assertEquals("9", new GdvSatzTyp("0220.010.9").getArtAsString());
+    }
+
+    @Test
+    public void testGetWagnisartAsString() {
+        assertEquals("0", new GdvSatzTyp("0220.010.0").getWagnisartAsString());
+        assertEquals("13", new GdvSatzTyp("0220.010.13").getWagnisartAsString());
+        assertEquals("13", new GdvSatzTyp("0221.010.13").getWagnisartAsString());
+        assertEquals("48", new GdvSatzTyp("0220.010.48").getWagnisartAsString());
+        assertEquals("48", new GdvSatzTyp("0221.010.48").getWagnisartAsString());
+        assertEquals("6", new GdvSatzTyp("0220.010.6").getWagnisartAsString());
+        assertEquals("6", new GdvSatzTyp("0221.010.6").getWagnisartAsString());
+        assertEquals("5", new GdvSatzTyp("0220.010.5").getWagnisartAsString());
+        assertEquals("5", new GdvSatzTyp("0221.010.5").getWagnisartAsString());
+        assertEquals("2", new GdvSatzTyp("0220.010.2").getWagnisartAsString());
+        assertEquals("2", new GdvSatzTyp("0221.010.2").getWagnisartAsString());
+        assertEquals("7", new GdvSatzTyp("0220.010.7").getWagnisartAsString());
+        assertEquals("7", new GdvSatzTyp("0221.010.7").getWagnisartAsString());
+        assertEquals("9", new GdvSatzTyp("0220.010.9").getWagnisartAsString());
+    }
+
+    @Test
+    public void testOfWagnisart() {
+        assertEquals(new GdvSatzTyp("0220.010.0"), new GdvSatzTyp(220, 10, 0));
+        assertEquals(new GdvSatzTyp("0220.010.13"), new GdvSatzTyp(220, 10, 13));
+        assertEquals(new GdvSatzTyp("0221.010.13"), new GdvSatzTyp(221, 10, 13));
+        assertEquals(new GdvSatzTyp("0220.010.48"), new GdvSatzTyp(220, 10, 48));
+        assertEquals(new GdvSatzTyp("0221.010.48"), new GdvSatzTyp(221, 10, 48));
+        assertEquals(new GdvSatzTyp("0220.010.6"), new GdvSatzTyp(220, 10, 6));
+        assertEquals(new GdvSatzTyp("0221.010.6"), new GdvSatzTyp(221, 10, 6));
+        assertEquals(new GdvSatzTyp("0220.010.9"), new GdvSatzTyp(220, 10, 9));
+    }
+
+    @Test
+    public void testGetArtWagnisart13() {
+        assertEquals("13", new GdvSatzTyp("220.10.1").getArtAsString());
+        assertEquals("13", new GdvSatzTyp("220.10.3").getArtAsString());
+        assertEquals("13", new GdvSatzTyp("0220.010.13").getWagnisartAsString());
+    }
+
+    @Test
+    public void testGetArtWagnisart48() {
+        assertEquals("48", new GdvSatzTyp("220.10.4").getArtAsString());
+        assertEquals("48", new GdvSatzTyp("220.10.8").getArtAsString());
+        assertEquals("48", new GdvSatzTyp("0220.010.48").getWagnisartAsString());
+    }
+
+    @Test
+    public void testHasArt() {
+        assertTrue(new GdvSatzTyp("0220.020.1").hasArt());
+        assertFalse(new GdvSatzTyp("0220.000").hasArt());
+    }
+
+    @Test
+    public void testToStringBausparen() {
+        assertEquals("0220.580.01", new GdvSatzTyp("0220.580.01").toString());
+        assertEquals("0220.580.2", new GdvSatzTyp("0220.580.2").toString());
+    }
+
+    @Test
+    public void testGetBausparenArt() {
+        assertEquals("01", new GdvSatzTyp("0220.580.01").getBausparenArtAsString());
+        assertEquals("2", new GdvSatzTyp("0220.580.2").getBausparenArtAsString());
+        assertEquals("", new GdvSatzTyp("0220.570").getBausparenArtAsString());
+    }
+
+    @Test
+    public void testOfBausparenArt() {
+        assertEquals(new GdvSatzTyp("0220.580.01"), new GdvSatzTyp(220, 580, 1));
+        assertEquals(new GdvSatzTyp("0220.580.2"), new GdvSatzTyp(220, 580, 2));
+        assertEquals(new GdvSatzTyp("0220.570"), new GdvSatzTyp(220, 570));
+    }
+
+    @Test
+    public void testGetKrankenFolgeNr() {
+        assertEquals(1, new GdvSatzTyp("0220.020.1").getKrankenFolgeNr());
+        assertEquals(2, new GdvSatzTyp("0220.020.2").getKrankenFolgeNr());
+        assertEquals(3, new GdvSatzTyp("0220.020.3").getKrankenFolgeNr());
+    }
+
+    @Test
+    public void testHasKrankenFolgeNr() {
+        assertTrue(new GdvSatzTyp("0220.020.1").hasKrankenFolgeNr());
+        assertTrue(new GdvSatzTyp("0220.020.2").hasKrankenFolgeNr());
+        assertTrue(new GdvSatzTyp("0220.020.3").hasKrankenFolgeNr());
+        assertFalse(new GdvSatzTyp("0220.000").hasKrankenFolgeNr());
+        assertFalse(new GdvSatzTyp("0220.580.01").hasKrankenFolgeNr());
+        assertFalse(new GdvSatzTyp("0220.580.2").hasKrankenFolgeNr());
+    }
+
+    @Test
+    public void testToStringKrankenFolgeNr() {
+        assertEquals("0220.020.1", new GdvSatzTyp("0220.020.1").toString());
+        assertEquals("0220.020.2", new GdvSatzTyp("0220.020.2").toString());
+        assertEquals("0220.020.3", new GdvSatzTyp("0220.020.3").toString());
+    }
+
+    @Test
+    public void testOfKrankenFolgeNr() {
+        assertEquals(new GdvSatzTyp(220, 20, 1), new GdvSatzTyp("0220.020.1"));
+        assertEquals(new GdvSatzTyp(220, 20, 2), new GdvSatzTyp("0220.020.2"));
+        assertEquals(new GdvSatzTyp(220, 20, 3), new GdvSatzTyp("0220.020.3"));
+    }
+
+    /**
+     * Fuer Satzart 210, 211 und 220 gibt es einen "Allgemeinen Satz", die
+     * "000" als Sparte hat. Daher sollte "0210" auf "0210.000" abgebildet
+     * werden (entsprechendes gilt fuer Satzart 211 und 220).
+     */
+    @Test
+    public void testAllgemeinerSatz() {
+        assertEquals(new GdvSatzTyp("0210.000"), new GdvSatzTyp("0210"));
+        assertEquals(new GdvSatzTyp("0211.000"), new GdvSatzTyp("0211"));
+        assertEquals(new GdvSatzTyp("0220.000"), new GdvSatzTyp("0220"));
+    }
+
+    /**
+     * GdvSatzTyp "0220.010" gibt es eigentlich nicht. In diesem Fall sollte die
+     * Wagnisart 0 genommen werden.
+     */
+    @Test
+    public void testLebenWagnisart0() {
+        assertEquals(new GdvSatzTyp("0220.010.0"), new GdvSatzTyp("0220.010"));
+        assertEquals("0220.010.0", new GdvSatzTyp("0220.010.0").toString());
+    }
+
+    @Test
+    public void testGdvSatzTyp100() {
+        GdvSatzTyp a = new GdvSatzTyp("0100");
+        GdvSatzTyp b = new GdvSatzTyp("0100.000");
+        assertEquals(a.toString(), b.toString());
+        ObjectTester.assertEquals(a, b);
+    }
+
+}
